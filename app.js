7b8380fff52127489ccb9b94aeffb27301663e41
Adesh <adeshramchurn@googlemail.com>
2021-07-09 12:55:45 +0100

    some demo changes

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log("hello new website");
+
+let scene, renderer;
+
+function init(){
+
+    scene = new THREE.scene();
+
+    scene.background = new THREE.Color(0xdddddd);
+
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 1, 5000);
+
+    hlight = new THREE.AmbientLight(0X404040, 100);
+    scene.add(hlight);
+
+    renderer = new THREE.WebGL1Renderer({antialias:true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+}
+
